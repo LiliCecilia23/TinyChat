158ee5c78298b74110c0c63aeef7f0616ed1959e
Lili Clift <liliclift@gmail.com>
2024-04-23 14:30:49 -0700

    All base code

--- tiny-chat/src/components/NavBar.js
+++ tiny-chat/src/components/NavBar.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const NavBar = () => {
-  const [user, setUser] = useState(false);
+    const [user] = useAuthState(auth);
 
-  const googleSignIn = () => {
-    setUser(true);
-  };
-
-  const signOut = () => {
-    setUser(false);
-  };
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithRedirect(auth, provider);
+      };
+      const signOut = () => {
+        auth.signOut();
+      };
 
   return (
     <nav className="nav-bar">
