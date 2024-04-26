86e566d3388f24365a2ca9d5009e49fbafefb47c
Lili Clift <liliclift@gmail.com>
2024-04-26 14:24:28 -0700

    testing

--- tiny-chat/src/firebase.js
+++ tiny-chat/src/firebase.js
@@ -8,6 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.REACT_APP_MEASUREMENT_ID)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
